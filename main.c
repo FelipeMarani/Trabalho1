2021824d04799d301910f09060e291e54591e521
FelipeMarani <felipejesusdasilva02@gmail.com>
2025-05-11 19:25:18 -0300

    update

--- main.c
+++ main.c
@@ -2,14 +2,159 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "mergesort.h"
+
+int merge(int v[], int l, int r)
+{
+    int i, j, k, m;
+    m = (l + r) / 2;      // cálculo do ponto médio do vetor
+    int size = r - l + 1; // tamanho do vetor a ser mesclado
+    i = l;                // inicialização do índice i para a metade esquerda
+    j = m + 1;            // inicialização do índice j para a metade direita
+    k = 0;                // inicialização do índice k para o vetor temporário
+    if (l >= r)
+    {
+        return 0; // caso o vetor tenha apenas um elemento ou esteja vazio, não há nada a fazer
+    }
+
+    int *temp = malloc(size * sizeof(int)); // alocação de memória para o vetor temporário
+    if (temp == NULL)                       // verificação se a alocação foi bem sucedida
+    {
+        printf("Erro ao alocar memória.\n");
+        return -1; // erro na alocação de memória
+    }
+
+    while (i <= m && j <= r)
+    {
+        if (v[i] < v[j])
+        {
+            temp[k++] = v[i++]; // se o elemento da esquerda for menor, adiciona ao vetor temporário
+        }
+        else
+        {
+            temp[k++] = v[j++]; // se o elemento da direita for menor, adiciona ao vetor temporário
+        }
+    }
+    while (i <= m)
+    {
+        temp[k++] = v[i++]; // adiciona os elementos restantes da esquerda ao vetor temporário
+    }
+    while (j <= r)
+    {
+        temp[k++] = v[j++]; // adiciona os elementos restantes da direita ao vetor temporário
+    }
+
+    for (i = l, k = 0; i <= r; i++, k++) // copia os elementos do vetor temporário de volta para o vetor original
+    {
+        v[i] = temp[k];
+    }
+
+    free(temp); // libera a memória alocada para o vetor temporário
+    return 0;   // retorna 0 para indicar que a função foi executada com sucesso
+}
+
+int mergesort(int v[], int l, int r)
+{
+    if (l >= r)
+    {
+        return 0; // caso o vetor tenha apenas um elemento ou esteja vazio, não há nada a fazer
+    }
+    int m = (l + r) / 2;    // cálculo do ponto médio do vetor
+    mergesort(v, l, m);     // chamada recursiva para a metade esquerda do vetor
+    mergesort(v, m + 1, r); // chamada recursiva para a metade direita do vetor
+    merge(v, l, r);         // chamada para a função de mesclagem
+    return 0;               // retorna 0 para indicar que a função foi executada com sucesso
+}
+
+void mergePassos(const char *comp1, const char *comp2, const char *ext){
+
+    FILE *op1 = fopen(comp1, "r"); // Abertura do arquivo em modo leitura
+    FILE *op2 = fopen(comp2, "r"); // Abertura do arquivo em modo leitura
+    FILE *out = fopen(ext, "w+"); // Abertura do arquivo em modo leitura e escrita
+
+    if (!op1 || !op2 || !out)// vericação se os arquivos foram abertos corretamente
+    {
+        printf("Erro ao abrir os arquivos.\n"); // caso não tenha sido, retorna erro e encerra o programa
+        exit(1);
+    }
+
+    int a, b;
+    int aV = fscanf(op1, "%d", &a); // leitura do primeiro arquivo
+    int bV = fscanf(op2, "%d", &b); // leitura do segundo arquivo
+    while (aV == 1 && bV == 1)// enquanto os dois arquivos tiverem números
+    {
+        if (a < b)
+        {
+            fprintf(out, "%d ", a); // se o número do primeiro arquivo for menor, escreve no arquivo de saída
+            aV = fscanf(op1, "%d", &a); // lê o próximo número do primeiro arquivo
+        }
+        else
+        {
+            fprintf(out, "%d ", b); // se o número do segundo arquivo for menor, escreve no arquivo de saída
+            bV = fscanf(op2, "%d", &b); // lê o próximo número do segundo arquivo
+        }
+        
+    }
+    while(aV == 1)//enquanto o primeiro arquivo tiver numeros
+    {
+        fprintf(out, "%d ", a);// escreve o número no arquivo de saída
+        aV = fscanf(op1, "%d", &a); // lê o próximo número do primeiro arquivo
+    }
+    while(bV == 1)//enquanto o segundo arquivo tiver numeros
+    {
+        fprintf(out, "%d ", b); // escreve o número no arquivo de saída
+        bV = fscanf(op2, "%d", &b); // lê o próximo número do segundo arquivo
+    }
+    fclose(op1); // fechamento do arquivo após a leitura
+    fclose(op2); // fechamento do arquivo após a leitura
+    fclose(out); // fechamento do arquivo após a leitura
+    printf("Arquivos mesclados com sucesso.\n"); // caso tenha sido, imprime mensagem de sucesso
+    printf("Arquivo de saída: out.txt"); // imprime o nome do arquivo de saída
+    
+}
+
+void mergeAll(int ttPassos){   // função para mesclar todos os arquivos
+     int t = 0;// inicialização do número de arquivos a serem mesclados
+     int qtd = ttPassos;// quantidade de arquivos a serem mesclados
+     char comp1[100], comp2[100], ext[100];// vetores de caracteres para os nomes dos arquivos
+
+     while (qtd > 1)
+     {
+        int nw = 0; // inicialização do número de arquivos a serem mesclados
+        for (int i = 0; i < qtd; i += 2)//loop para mesclar os arquivos
+        {
+            sprintf(comp1, "/home/fels/Documentos/POD/Trabalho1/passo%d.txt", t == 0 ? "passo" : "merge", i);// criação do nome do arquivo a ser aberto, de acordo com o número do passo
+            sprintf(comp2, "/home/fels/Documentos/POD/Trabalho1/passo%d.txt", t == 0 ? "passo" : "merge", i + 1);// criação do nome do arquivo a ser aberto, de acordo com o número do passo
+
+            FILE *tst = fopen(comp2, "r"); // Abertura do arquivo em modo leitura
+            if (!tst)// verificação se o arquivo foi aberto de maneira correta
+            {
+              fclose(fopen(comp1, "r"));// fechamento do arquivo
+              sprintf(ext, "/home/fels/Documentos/POD/Trabalho1/Merge%d.txt", nw);// criação do nome do arquivo a ser aberto, de acordo com o número do passo
+              rename(comp1, ext);// renomeia o arquivo
+              continue;// continua o loop
+            }
+            fclose(tst);// fechamento do arquivo
+            sprintf(ext, "/home/fels/Documentos/POD/Trabalho1/Merge%d.txt", nw++);// criação do nome do arquivo a ser aberto, de acordo com o número do passo
+            mergePassos(comp1, comp2, ext);// chamada da função mergePassos para mesclar os arquivos
+        }
+        qtd = (qtd + 1) / 2; // atualização da quantidade de arquivos a serem mesclados
+        t++;// incremento do número de arquivos a serem mesclados
+
+     }
+
+     rename("/home/fels/Documentos/POD/Trabalho1/Merge0.txt", "home/fels/Documentos/POD/Trabalho1/out.txt");// renomeia o arquivo final
+     printf("Arquivo final: out.txt\n"); // imprime o nome do arquivo final
+     printf("Mesclagem concluída com sucesso.\n"); // imprime mensagem de sucesso
+     
+}
 
 // inicialização da função main
 int main()
 {
 
-    char passo[100];        // criação do vetor de caracteres passo com tamanho 100             // inicialização do vetor v com o tamanho definido por m
-    int num = 0, ch, i = 0; // inicialização das variáveis num, ch e i
+    char passo[100]; // criação do vetor de caracteres passo com tamanho 100             // inicialização do vetor v com o tamanho definido por m
+    int num = 0, ch, i = 0;
+    int numTxt = 0; // inicialização das variáveis num, ch e i
 
     FILE *fp = fopen("/home/fels/Documentos/POD/Trabalho1/dados.txt", "r"); // Abertura do arquivo em modo leitura
     if (fp == NULL)
@@ -22,7 +167,7 @@ int main()
         printf("Arquivo de dados aberto com sucesso.\n"); // caso tenha sido, imprime mensagem de sucesso
     }
 
-    int m, cn = 0;                   // criação da variável m
+    int m;                           // criação da variável m
     printf("Digite o valor de m: "); // solicitação do valor de m ao usuário para definir o tamanho do vetor
     scanf("%d", &m);
     if (m <= 0)
@@ -33,15 +178,7 @@ int main()
     }
     int v[m];
 
-    while ((fp != NULL) ) // leitura do arquivo
-    {
-        
-        
-    }
-    
-
-    while ((ch = fgetc(fp)) != EOF && i < cn) // leitura
-    // fgetc lê um caractere do arquivo e retorna o valor lido
+    while ((ch = fgetc(fp)) != EOF) // leitura do arquivo
     {
         if (ch >= '0' && ch <= '9')
         {                                // verificação se o caractere lido é um número
@@ -52,23 +189,32 @@ int main()
             v[i++] = num;
             num = 0;
         }
-    }
-    fclose(fp); // fechamento do arquivo após a leitura
 
-    int numTxt = 0; // inicialização da variável numTxt
-
-    while (numTxt < (m * 2)) // while usado para criação dos arquivos sendo o dobro da variavel m fornecida
-    {
-        sprintf(passo, "/home/fels/Documentos/POD/Trabalho1/passo%d.txt", numTxt); // criação do nome do arquivo a ser aberto, de acordo com o número do passo
-
-        FILE *nwfp = (fopen(passo, "w+")); // abertura do arquivo em modo leitura e escrita
-        if (nwfp == NULL)
-        {                                                  // verificação se o arquivo foi aberto de maneira correta
-            printf("Erro ao abrir o arquivo %s\n", passo); // caso não tenha sido, retorna erro e encerra o programa
-            exit(1);
+        if (i == m)
+        {
+            mergesort(v, 0, m - 1);                                                      // chamada da função mergesort para ordenar o vetor
+            sprintf(passo, "/home/fels/Documentos/POD/Trabalho1/passo%d.txt", numTxt++); // criação do nome do arquivo a ser aberto, de acordo com o número do passo
+            FILE *nw = fopen(passo, "w+");                                               // abertura do arquivo em modo leitura e escrita
+            for (int j = 0; j < i; j++)
+            {
+                fprintf(nw, "%d ", v[j]); // impressão do vetor ordenado
+            }
+            fclose(nw); // fechamento do arquivo após a leitura
+            i = 0;      // reinicialização do índice i para o próximo vetor
         }
-        fclose(nwfp); // fechamento do arquivo após a leitura
-        // A função sprintf é usada para formatar a string e armazená-la na variável passo
-        numTxt++; // incremento da variável numTxt
     }
+    if (i > 0) // verificação se o vetor tem elementos restantes
+    {
+        mergesort(v, 0, i - 1);                                                      // chamada da função mergesort para ordenar o vetor
+        sprintf(passo, "/home/fels/Documentos/POD/Trabalho1/passo%d.txt", numTxt++); // criação do nome do arquivo a ser aberto, de acordo com o número do passo
+        FILE *nw = fopen(passo, "w+");                                               // abertura do arquivo em modo leitura e escrita
+        for (int j = 0; j < i; j++)
+        {
+            fprintf(nw, "%d ", v[j]); // impressão do vetor ordenado
+        }
+    }
+
+    fclose(fp); // fechamento do arquivo após a leitura
+    mergeAll(numTxt); // chamada da função mergeAll para mesclar os arquivos
+    return 0;   // retorno da função main
 }
--- mergesort.h
+++
@@ -1,6 +0,0 @@
-#ifndef MERGESORT_H
-#define MERGESORT_H
-
-int mergesort(int v[], int l, int r);
-
-#endif // MERGESORT_H
