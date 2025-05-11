7dbf261afcf5bf71065128e3d5bb4d44807f02db
FelipeMarani <felipejesusdasilva02@gmail.com>
2025-05-11 15:13:56 -0300

    update

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 int main()
 {
 
-    char passo[100]; // criação do vetor de caracteres passo com tamanho 100
+    char passo[100];        // criação do vetor de caracteres passo com tamanho 100             // inicialização do vetor v com o tamanho definido por m
+    int num = 0, ch, i = 0; // inicialização das variáveis num, ch e i
 
     FILE *fp = fopen("/home/fels/Documentos/POD/Trabalho1/dados.txt", "r"); // Abertura do arquivo em modo leitura
     if (fp == NULL)
@@ -16,25 +17,30 @@ int main()
         printf("Erro ao abrir o arquivo.\n"); // caso não tenha sido, retorna erro e encerra o programa
         exit(1);
     }
-    else{
+    else
+    {
         printf("Arquivo de dados aberto com sucesso.\n"); // caso tenha sido, imprime mensagem de sucesso
     }
 
-    int m;                           // criação da variável m
+    int m, cn = 0;                   // criação da variável m
     printf("Digite o valor de m: "); // solicitação do valor de m ao usuário para definir o tamanho do vetor
     scanf("%d", &m);
     if (m <= 0)
     {
         printf("Valor inválido para m. O programa será encerrado.\n"); // verificação se o valor de m é válido
-        fclose(fp); // fechamento do arquivo
-        exit(1);    // caso não seja, retorna erro e encerra o programa
+        fclose(fp);                                                    // fechamento do arquivo
+        exit(1);                                                       // caso não seja, retorna erro e encerra o programa
+    }
+    int v[m];
+
+    while ((fp != NULL) ) // leitura do arquivo
+    {
+        
+        
     }
     
 
-    int v[m];               // inicialização do vetor v com o tamanho definido por m
-    int num = 0, ch, i = 0; // inicialização das variáveis num, ch e i
-
-    while ((ch = fgetc(fp)) != EOF && i < m) // leitura do arquivo até preencher o vetor ou até finalizar o arquivo
+    while ((ch = fgetc(fp)) != EOF && i < cn) // leitura
     // fgetc lê um caractere do arquivo e retorna o valor lido
     {
         if (ch >= '0' && ch <= '9')
@@ -51,13 +57,13 @@ int main()
 
     int numTxt = 0; // inicialização da variável numTxt
 
-    while (numTxt < (m * 2))//while usado para criação dos arquivos sendo o dobro da variavel m fornecida
+    while (numTxt < (m * 2)) // while usado para criação dos arquivos sendo o dobro da variavel m fornecida
     {
         sprintf(passo, "/home/fels/Documentos/POD/Trabalho1/passo%d.txt", numTxt); // criação do nome do arquivo a ser aberto, de acordo com o número do passo
 
         FILE *nwfp = (fopen(passo, "w+")); // abertura do arquivo em modo leitura e escrita
         if (nwfp == NULL)
-        {                                            // verificação se o arquivo foi aberto de maneira correta
+        {                                                  // verificação se o arquivo foi aberto de maneira correta
             printf("Erro ao abrir o arquivo %s\n", passo); // caso não tenha sido, retorna erro e encerra o programa
             exit(1);
         }
